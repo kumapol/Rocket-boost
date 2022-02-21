004ce8679d821fedb49cbe8e2876d46b5524fc30
kumapol <kubapolny@gmail.com>
2022-02-21 23:32:00 +0100

    Scene menagment+ load next scenes+ reseting lvls

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
     int numberOfHits;
+    [SerializeField] int numberOfLives=2;
 private void OnCollisionEnter(Collision other) {
     
     switch(other.gameObject.tag)
@@ -11,25 +12,52 @@ private void OnCollisionEnter(Collision other) {
     case "Fuel":
         Debug.Log("Fuel added!");
         break;
+
       case "Finish":
-        Debug.Log("You win!");
+        Debug.Log("You win this level");
+        Invoke("LoadNextLvl",2f);
         break;
+
     case "Friendly":
         Debug.Log("Landing...");
         break;
+
     default:
         numberOfHits++;
         Debug.Log("Bump! + " + numberOfHits);
-        if(numberOfHits>=2)
-        {
-        ResetScene();
-        }
-        break;
+
+            if(numberOfHits>=numberOfLives)
+            {
+            StartCrashSequence();
+            }
+            break;
     }
 }
+void StartCrashSequence(){
+   GetComponent<Movement>().enabled=false;
+ GetComponent<AudioSource>().enabled=false;
+
+    Invoke("ResetScene",2f);
+}
+
 void ResetScene(){
     int currentScene=SceneManager.GetActiveScene().buildIndex;
 SceneManager.LoadScene(currentScene);
+}
+void LoadNextLvl()
+{
+    int currentScene=SceneManager.GetActiveScene().buildIndex;
+    int maxScenes=SceneManager.sceneCount;
+Debug.Log(maxScenes + "   "+currentScene);
+    if(maxScenes<=currentScene)
+    {
+    SceneManager.LoadScene("LVL1");  
+    Debug.Log("You won! Nongrats my dude =D");
+    }
+    else
+    {
+    SceneManager.LoadScene(currentScene+1);
+    }
 }
 
 }
